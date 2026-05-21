9d7d5b7288a94c3844e40a0b17c5b64b3e451eb8
Yosber Guerrero <yosbergc@gmail.com>
2026-05-20 22:43:36 -0400

    Added methods

--- lib/hooks/observer.ts
+++ lib/hooks/observer.ts
@@ -1,5 +1,30 @@
+type Listener = (text: string) => void
+
 class PenduloObserver {
-    private dropdownList = new Map()
+    private dropdownList = new Map<string, Listener>()
     
-    
+    subscribe(id: string, listener: Listener) {
+        this.dropdownList.set(id, listener)
+
+        return () => {
+            this.off(id)
+        }
+    }
+
+    off(id: string) {
+        if (this.dropdownList.has(id)){
+            this.dropdownList.delete(id)
+        }
+    }
+
+    emit(id: string, payload: string){
+        if (this.dropdownList.has(id)) {
+            const listener = this.dropdownList.get(id)
+            if (listener) {
+                listener(payload)
+            }
+        }
+    }
 }
+
+export const penduloObserverInstance = new PenduloObserver()
