65ea9b1c8bdb740122ea9a93402575e713cbd76a
Yosber Guerrero <yosbergc@gmail.com>
2026-05-27 09:41:09 -0400

    Added onclick method to items

--- lib/components/Item.tsx
+++ lib/components/Item.tsx
@@ -1,7 +1,13 @@
 import styles from './Item.module.css'
-export function Item({ text, Icon } : { text: string, Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>> }) {
+
+interface ItemType {
+    text: string
+    Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+    onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+}
+export function Item({ text, Icon, onClick } : ItemType) {
     return (
-        <section className={styles.penduloItemComponent}>
+        <section className={styles.penduloItemComponent} onClick={onClick}>
             { Icon && <Icon width={14} height={14}/> }
             <p>{text}</p>
         </section>
